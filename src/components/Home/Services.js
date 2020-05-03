becc8d57a7ab8ea3d7a0daf0ebbb677d7e315a00
roberto chupin <Z11@users.noreply.github.com>
2020-05-03 14:25:24 -0500

    added prismic to Services.js

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -1,22 +1,62 @@
 import React from "react"
 import Title from "../Title"
 import styles from "../../css/services.module.css"
-import services from "../../constants/services"
+import { FaHeart, FaBusinessTime } from "react-icons/fa"
+import { GiGlassCelebration } from "react-icons/gi"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getServices = graphql`
+  query {
+    prismic {
+      home_page(uid: "home_page_slug", lang: "en-us") {
+        services_main_title
+        service_1_title
+        service_1_text
+        service_2_title
+        service_2_text
+        service_3_title
+        service_3_text
+      }
+    }
+  }
+`
 
 const Services = () => {
+  const {
+    services_main_title,
+    service_1_title,
+    service_1_text,
+    service_2_title,
+    service_2_text,
+    service_3_title,
+    service_3_text,
+  } = useStaticQuery(getServices).prismic.home_page
+
   return (
     <section className={styles.services} id="servicesStevie">
-      <Title title="Our Services" position="center" />
+      <Title title={services_main_title[0].text} position="center" />
       <div className={styles.center}>
-        {services.map((item, index) => {
-          return (
-            <article key={index} className={styles.service}>
-              <span>{item.icon}</span>
-              <h4>{item.title}</h4>
-              <p>{item.text}</p>
-            </article>
-          )
-        })}
+        <article key={1} className={styles.service}>
+          <span>
+            <FaHeart />
+          </span>
+          <h4>{service_1_title[0].text}</h4>
+          <p>{service_1_text[0].text}</p>
+        </article>
+        <article key={2} className={styles.service}>
+          <span>
+            <GiGlassCelebration />
+          </span>
+          <h4>{service_2_title[0].text}</h4>
+          <p>{service_2_text[0].text}</p>
+        </article>
+        <article key={3} className={styles.service}>
+          <span>
+            <FaBusinessTime />
+          </span>
+          <h4>{service_3_title[0].text}</h4>
+          <p>{service_3_text[0].text}</p>
+        </article>
       </div>
     </section>
   )
--- src/constants/services.js
+++ src/constants/services.js
@@ -1,21 +1,37 @@
 import React from "react"
 import { FaHeart, FaBusinessTime } from "react-icons/fa"
 import { GiGlassCelebration } from "react-icons/gi"
+import { graphql } from "gatsby"
 
 export default [
   {
     icon: <FaHeart />,
-    title: "Weddings",
-    text: "Ready to make your wedding day amazing.",
+    title: "service_1_title[0].text",
+    text: "service_1_text[0].text",
   },
   {
     icon: <GiGlassCelebration />,
-    title: "Private Events",
-    text: "We will capture your memories.",
+    title: "service_2_title[0].text",
+    text: "service_2_text[0].text",
   },
   {
     icon: <FaBusinessTime />,
-    title: "B2B",
-    text: "Ready to do business.",
+    title: "service_3_title[0].text",
+    text: "service_3_text[0].text",
   },
 ]
+
+export const getServices = graphql`
+  query {
+    prismic {
+      home_page(uid: "home_page_slug", lang: "en-us") {
+        service_1_title
+        service_1_text
+        service_2_title
+        service_2_text
+        service_3_title
+        service_3_text
+      }
+    }
+  }
+`
