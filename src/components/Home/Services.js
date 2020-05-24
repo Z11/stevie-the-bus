6975b0f9ab4ed97fa569d4ec3678735472a5c90a
roberto chupin <Z11@users.noreply.github.com>
2020-05-24 00:32:28 -0500

    added images to services and service.js const file for icons

--- src/components/Home/Services.js
+++ src/components/Home/Services.js
@@ -1,8 +1,7 @@
 import React from "react"
 import Title from "../Title"
 import styles from "../../css/services.module.css"
-import { FaHeart, FaBusinessTime } from "react-icons/fa"
-import { GiGlassCelebration } from "react-icons/gi"
+import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 
 const getServices = graphql`
@@ -10,10 +9,34 @@ const getServices = graphql`
     prismic {
       home_page(uid: "home_page_slug", lang: "en-us") {
         services_main_title
+        service_1_image
+        service_1_imageSharp {
+          childImageSharp {
+            fixed(width: 150) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
         service_1_title
         service_1_text
+        service_2_image
+        service_2_imageSharp {
+          childImageSharp {
+            fixed(width: 150) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
         service_2_title
         service_2_text
+        service_3_image
+        service_3_imageSharp {
+          childImageSharp {
+            fixed(width: 150) {
+              ...GatsbyImageSharpFixed_withWebp_noBase64
+            }
+          }
+        }
         service_3_title
         service_3_text
       }
@@ -24,10 +47,13 @@ const getServices = graphql`
 const Services = () => {
   const {
     services_main_title,
+    service_1_imageSharp,
     service_1_title,
     service_1_text,
+    service_2_imageSharp,
     service_2_title,
     service_2_text,
+    service_3_imageSharp,
     service_3_title,
     service_3_text,
   } = useStaticQuery(getServices).prismic.home_page
@@ -38,21 +64,30 @@ const Services = () => {
       <div className={styles.center}>
         <article key={1} className={styles.service}>
           <span>
-            <FaHeart />
+            <Img
+              fixed={service_1_imageSharp.childImageSharp.fixed}
+              alt="desktop bus logo"
+            />
           </span>
           <h4>{service_1_title[0].text}</h4>
           <p>{service_1_text[0].text}</p>
         </article>
         <article key={2} className={styles.service}>
           <span>
-            <GiGlassCelebration />
+            <Img
+              fixed={service_2_imageSharp.childImageSharp.fixed}
+              alt="desktop bus logo"
+            />
           </span>
           <h4>{service_2_title[0].text}</h4>
           <p>{service_2_text[0].text}</p>
         </article>
         <article key={3} className={styles.service}>
           <span>
-            <FaBusinessTime />
+            <Img
+              fixed={service_3_imageSharp.childImageSharp.fixed}
+              alt="desktop bus logo"
+            />
           </span>
           <h4>{service_3_title[0].text}</h4>
           <p>{service_3_text[0].text}</p>
