06aedbbd28f4e06d33dbee6f7b10bcb6b8c7a43c
roberto chupin <Z11@users.noreply.github.com>
2020-05-25 12:39:17 -0500

    lowered image sizes

--- src/components/Gallery/useGallery.js
+++ src/components/Gallery/useGallery.js
@@ -16,7 +16,7 @@ const useGallery = () => {
                       name
                       publicURL
                       childImageSharp {
-                        fluid(quality: 100, maxWidth: 500) {
+                        fluid(quality: 100, maxWidth: 250) {
                           ...GatsbyImageSharpFluid_withWebp_tracedSVG
                         }
                       }
