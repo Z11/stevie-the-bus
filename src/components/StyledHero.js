30c228e76f205bbe72684c35095b0ee7f2b4daaa
roberto chupin <Z11@users.noreply.github.com>
2020-06-06 01:22:14 -0500

    fit hero image on mobile

--- src/components/StyledHero.js
+++ src/components/StyledHero.js
@@ -12,7 +12,6 @@ const StyledHero = ({ img, className, children, home }) => {
 
 export default styled(StyledHero)`
   /* min-height controls height of hero image and impacts width */
-  min-height: ${props => (props.home ? "calc(95vh - 60px)" : "50vh")};
   background: "none";
   background-position: center;
   background-size: cover;
@@ -20,4 +19,12 @@ export default styled(StyledHero)`
   display: flex;
   justify-content: center;
   align-items: center;
+  /* mobile */
+  @media (max-width: 800px) {
+    min-height: ${"50vh"};
+  }
+  /* desktop */
+  @media (min-width: 800px) {
+    min-height: ${props => (props.home ? "calc(95vh - 60px)" : "50vh")};
+  }
 `
