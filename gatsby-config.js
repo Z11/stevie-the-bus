32eeba2fc38cc196df85126d8a5c62d02cf067b6
roberto chupin <Z11@users.noreply.github.com>
2020-06-27 12:12:10 -0500

    added preact, not sure how this works but we shall see

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     siteUrl: `https://www.steviethephotobus.com/`,
   },
   plugins: [
+    `gatsby-plugin-preact`,
     `gatsby-plugin-sitemap`,
     {
       resolve: "gatsby-plugin-robots-txt",
