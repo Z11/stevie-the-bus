807b515cd3740bb2af7f282b4cbb5678bf875ea8
roberto chupin <Z11@users.noreply.github.com>
2020-07-02 23:32:13 -0500

    align navbar

--- src/components/Gallery/Grid.js
+++ src/components/Gallery/Grid.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const Grid = styled.div`
-  padding: 1rem 1rem;
+  padding: 0rem 1rem 1rem 1rem;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
   grid-auto-rows: 16rem;
