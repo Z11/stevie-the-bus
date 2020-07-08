003a8a4ea8fc121c81cd6a6940390864c7351e2a
roberto chupin <Z11@users.noreply.github.com>
2020-07-08 16:55:41 -0500

    added @media to gallery

--- src/components/Gallery/Grid.js
+++ src/components/Gallery/Grid.js
@@ -1,14 +1,30 @@
 import styled from "styled-components"
 
 const Grid = styled.div`
-  padding: 0rem 1rem 1rem 1rem;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
-  grid-auto-rows: 16rem;
-  grid-gap: 0.5rem;
-  @media (min-width: 1280px) {
+
+  /*  Small devices (landscape phones) */
+  @media screen and (min-width: 300px) and (max-width: 600px) {
+    padding: 0rem 0.3rem 0.3rem 0.3rem;
+    grid-template-columns: repeat(auto-fit, minmax(12rem, 1fr));
+    grid-auto-rows: 12rem;
+    grid-gap: 0.2rem;
+  }
+
+  /*  Large devices (large desktops) */
+  @media screen and (min-width: 600px) and (max-width: 1600px) {
+    padding: 0rem 0.5rem 0.5rem 0.5rem;
+    grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+    grid-auto-rows: 16rem;
+    grid-gap: 0.3rem;
+  }
+
+  /*  Extra large devices (extra large desktops) */
+  @media (min-width: 1600px) {
+    padding: 0rem 1rem 1rem 1rem;
     grid-template-columns: repeat(auto-fit, minmax(24rem, 1fr));
     grid-auto-rows: 24rem;
+    grid-gap: 0.5rem;
   }
 `
 
