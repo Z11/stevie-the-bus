f93cda1afb3cca0966f3350ab8def4874762cc1f
roberto chupin <Z11@users.noreply.github.com>
2020-07-14 23:07:39 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,23 @@ module.exports = {
     siteUrl: `https://www.steviethephotobus.com/`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: process.env.GOOGLE_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: "gatsby-plugin-robots-txt",
