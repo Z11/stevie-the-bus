b8820eee555dd3d25c52ddd8bad52f171a23f05f
Roberto Chupin <roberto.chupin.dev@gmail.com>
2021-03-09 18:37:31 -0600

    removed categories

--- src/components/Faq/FaqDetails.js
+++ src/components/Faq/FaqDetails.js
@@ -24,32 +24,17 @@ const getFaqDetails = graphql`
         open_graph_image
       }
     }
-    prismic {
-      allQuestion_categorys {
-        edges {
-          node {
-            category_name
-          }
-        }
-      }
-    }
   }
 `
 
 const Faq = () => {
   const {
-    allQuestion_categorys: { edges: edgeCategories },
     page: { pageTitle, questions },
   } = useStaticQuery(getFaqDetails).prismic
-
-  const listOfCategories = edgeCategories.map((x, index) => {
-    return { id: index, category: x.node.category_name[0].text }
-  })
 
   const listOfFAQs = questions.map((x, index) => {
     return {
       id: index,
-      category: x.category.categoryName[0].text,
       question: x.question[0].text,
       answer: x.answer[0].text,
     }
@@ -62,15 +47,8 @@ const Faq = () => {
       <Title title={pageTitle[0].text} position="center" />
       <div className={styles.faqCenter}>
         <div className={styles.faqDetails}>
-          {listOfCategories.map(item => (
-            <div
-              className={styles.faqContainer}
-              key={item.id}
-              id={item.category.replace(/\s+/g, "")}
-            >
-              <div className={styles.faqCategoryTitle}>{item.category}</div>
+            <div className={styles.faqContainer}>
               {listOfFAQs
-                .filter(x => x.category === item.category)
                 .map(item => (
                   <Accordion
                     key={item.id}
@@ -81,7 +59,6 @@ const Faq = () => {
                   />
                 ))}
             </div>
-          ))}
         </div>
       </div>
     </section>
